aac677ca70fea20f202ba9856fc673de2e8fba0d
Jonathan Bobrow <jbobrow@gmail.com>
2016-01-12 20:46:47 -0500

    more of the same, have to communicate the actions performed on the intersections

--- methods.js
+++ methods.js
@@ -1,7 +1,11 @@
 /**
  * @author Benjamin Berman
  * © 2014 All Rights Reserved
+ *
+ * !!!!IMPORTANT!!!!
+ * NOTE: this is only how a person modifies the game state
  **/
+
 Meteor.methods({
     matchMakeAndJoin: function () {
         if (!this.userId) {
@@ -59,5 +63,35 @@ Meteor.methods({
         // TODO: Security
 
         return Sanitaire.addQuarantine(gameId, position, intersectionId);
+    },
+
+    startConstruction: function(gameId, position, intersectionId, startTime) {
+        if (!this.userId) {
+            throw new Meteor.Error(403, 'Permission denied.');
+        }
+
+        return Sanitaire.addConstructionMessageToLog(gameId, playerId, messageType, time);
+    },
+
+    stopConstruction: function(gameId, position, intersectionId, stopTime) {
+        if (!this.userId) {
+            throw new Meteor.Error(403, 'Permission denied.');
+        }
+
+    },
+
+    startDeconstruciton: function(gameId, position, intersectionId, startTime) {
+        if (!this.userId) {
+            throw new Meteor.Error(403, 'Permission denied.');
+        }
+
+    },
+
+    stopDeconstruciton: function(gameId, position, intersectionId, stopTime) {
+        if (!this.userId) {
+            throw new Meteor.Error(403, 'Permission denied.');
+        }
+
     }
+
 });
