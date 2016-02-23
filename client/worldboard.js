a16a7da673373cd3f0dead1d312d035770ab2fc8
Jonathan Bobrow <jbobrow@gmail.com>
2016-02-22 23:53:30 -0500

    make it easy to change the injury time

--- client/worldboard.js
+++ client/worldboard.js
@@ -873,7 +873,7 @@ Template.worldBoard.onRendered(function () {
                         // keep the player stunned for 5 seconds since they were touched
                         // NOTE: if the time when they were touched has been updated to a
                         // more recent time, they will be stunned for longer
-                        if ((currentTime - localPlayerState.health.timeWhenTouchedByPatientZero)<5000){
+                        if ((currentTime - localPlayerState.health.timeWhenTouchedByPatientZero) < SanitairePatientZero.INJURY_TIME){
                             stunPlayer(gameId, sprite);
                             isPlayerInjured = true;
                         } else {
