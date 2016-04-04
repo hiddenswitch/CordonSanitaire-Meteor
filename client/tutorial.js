1f6f9ec4cd5cd3c76d4dd04c3bcc12c1d791fdfa
Jonathan Bobrow <jbobrow@gmail.com>
2016-04-04 17:23:15 -0400

    small change to tutorial for button handling

--- client/tutorial.js
+++ client/tutorial.js
@@ -3,6 +3,14 @@
  * © 2014 All Rights Reserved
  **/
 Template.tutorial.events({
+    'click #nextButton': function () {
+        // load the next image if available
+        // update the UI to show correct buttons
+    },
+    'click #prevButton': function () {
+        // load the previous image if available
+        // update the UI to show correct buttons
+    },
     'click #finish': function () {
         //Meteor.call('finishTutorial');
         Router.go('mainmenu');
